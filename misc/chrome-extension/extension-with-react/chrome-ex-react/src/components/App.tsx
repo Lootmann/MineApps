6a738f886a9bca9672a95e2b8189e698d4034194
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-10-28 13:13:22 +0900

    misc: chrome-extension - how to use chrome.storage - now I can save data for locally

--- misc/chrome-extension/extension-with-react/chrome-ex-react/src/components/App.tsx
+++ misc/chrome-extension/extension-with-react/chrome-ex-react/src/components/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 export function App() {
+  // chrome storage
+  chrome.storage.sync.set({ id: 1 }, function () {
+    console.log(`value is set to 1`);
+  });
+
+  chrome.storage.sync.get(["id"], function (result) {
+    console.log("result = ", result.id);
+  });
+
   return (
     <main id="main">
       <h1>hello world</h1>
