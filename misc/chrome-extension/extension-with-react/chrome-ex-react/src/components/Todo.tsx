4872864a20fd42c6e38b74662954fdb6778c197b
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-10-29 19:43:17 +0900

    misc: chrome-extension - todo form and show todos

--- misc/chrome-extension/extension-with-react/chrome-ex-react/src/components/Todo.tsx
+++ misc/chrome-extension/extension-with-react/chrome-ex-react/src/components/Todo.tsx
@@ -3,18 +3,58 @@ import React from "react";
 import "../styles/todo.css";
 
 export function TodoList() {
+  const [todos, setTodos] = React.useState<string[]>(["prev", "todo", "next"]);
+
   return (
     <ul className="todo-list">
       <AddTodo />
-      <Todo />
+      <Todo todos={todos} />
     </ul>
   );
 }
 
-export function Todo() {
-  return <li className="todo">Todo</li>;
+export function AddTodo() {
+  const todoRef = React.useRef(null);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <li className="addtodo">
+      <form
+        action=""
+        method="get"
+        id="add-form"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="input new todo :^)"
+          ref={todoRef}
+          autoFocus
+        />
+      </form>
+    </li>
+  );
 }
 
-export function AddTodo() {
-  return <li className="addtodo">AddTodo</li>;
+type TodoProps = {
+  todos: string[];
+};
+
+export function Todo(props: TodoProps) {
+  return (
+    <>
+      {props.todos.map((todo) => {
+        return (
+          <li className="todo" key={todo}>
+            {todo}
+          </li>
+        );
+      })}
+    </>
+  );
 }
