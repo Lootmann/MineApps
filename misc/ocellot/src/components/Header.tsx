eecb2bb774ab46463e0230e5b2cc3540bfee76cf
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-10-31 07:51:53 +0900

    misc: ocellot - add Header

--- misc/ocellot/src/components/Header.tsx
+++ misc/ocellot/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import "../styles/header.css";
+import { HeaderProps } from "../models/Types";
 
-export function Header() {
+export function Header(props: HeaderProps) {
   return (
     <header id="header">
-      <h1>Header</h1>
+      <h1 className="h1">Header</h1>
+      <p className="turn">Turn : {props.turn ? "White" : "Black"}</p>
     </header>
   );
 }
