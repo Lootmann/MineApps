0a8de9a3524ed4511d6c3bc8a36960c3c8093f52
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-11-01 20:41:10 +0900

    misc: base64 en/decoding imple

--- misc/base64encoding/mybase64.py
+++ misc/base64encoding/mybase64.py
@@ -13,7 +13,7 @@ class Base64:
 
         if 0 <= idx <= 25:
             return ascii_uppercase[idx]
-        elif 26 <= idx <= 52:
+        elif 26 <= idx <= 51:
             return ascii_lowercase[idx - 26]
         elif 52 <= idx <= 61:
             return digits[idx - 52]
@@ -57,78 +57,53 @@ class Base64:
         raise ValueError("This char: {} is NOT in base64encode map".format(char))
 
     @classmethod
+    def convert_bit_series(cls, string: str) -> list:
+        # convet string to str bit
+        bits = []
+        for c in string:
+            bits.append("{:0>8s}".format(bin(ord(c))[2:]))
+
+        # and divides the bits string into 6-bit segments
+        concat = "".join(bits)
+
+        if len(concat) % 6 != 0:
+            concat += "0" * (6 - len(concat) % 6)
+
+        return [concat[6 * i : 6 * i + 6] for i in range(len(concat) // 6)]
+
+    @classmethod
+    def convert_bit_to_base64encode(cls, splitted_bit: list) -> str:
+        # convert bit to encoding char with base64 mapping
+        encode_string = "".join([Base64.bit2char(bit) for bit in splitted_bit])
+
+        # padding
+        if len(encode_string) % 4 != 0:
+            encode_string += "=" * (4 - len(encode_string) % 4)
+
+        return encode_string
+
+    @classmethod
     def base64encoding(cls, src: str) -> str:
-        return ""
+        # string to bit
+        bit_series = cls.convert_bit_series(src)
+
+        # bit to base64encoding
+        encode_string = cls.convert_bit_to_base64encode(bit_series)
+
+        return encode_string
 
     @classmethod
     def base64decoding(cls, src: str) -> str:
-        return ""
+        # strip '=' by last bit
+        stripped = src.rstrip("=")
 
+        # convert to base64 bit
+        base64_chars = [cls.char2bit(char) for char in list(stripped)]
 
-"""
-000000	A
-000001	B
-000010	C
-000011	D
-000100	E
-000101	F
-000110	G
-000111	H
-001000	I
-001001	J
-001010	K
-001011	L
-001100	M
-001101	N
-001110	O
-001111	P
-010000	Q
-010001	R
-010010	S
-010011	T
-010100	U
-010101	V
-010110	W
-010111	X
-011000	Y
-011001	Z
-011010	a
-011011	b
-011100	c
-011101	d
-011110	e
-011111	f
-ビット列	Base64文字
-100000	g
-100001	h
-100010	i
-100011	j
-100100	k
-100101	l
-100110	m
-100111	n
-101000	o
-101001	p
-101010	q
-101011	r
-101100	s
-101101	t
-101110	u
-101111	v
-110000	w
-110001	x
-110010	y
-110011	z
-110100	0
-110101	1
-110110	2
-110111	3
-111000	4
-111001	5
-111010	6
-111011	7
-111100	8
-111101	9
-111110	+
-111111	/
-"""
+        # concat 8-bit segments
+        concat = "".join(base64_chars)
+
+        splitted = [concat[8 * i : 8 * i + 8] for i in range(len(concat) // 8)]
+
+        # bit to char
+        return "".join(chr(int(bits, 2)) for bits in splitted)
--- misc/base64encoding/test_base64.py
+++ misc/base64encoding/test_base64.py
@@ -38,3 +38,63 @@ class TestBase64EncodingUtils:
     def test_char2bit(self, char: str, want: str):
         got = Base64.char2bit(char)
         assert got == want
+
+    @pytest.mark.parametrize(
+        "string,want",
+        [
+            ["a", ["011000", "010000"]],
+            ["ab", ["011000", "010110", "001000"]],
+            ["abc", ["011000", "010110", "001001", "100011"]],
+            [
+                "abcdefg",
+                [
+                    "011000",
+                    "010110",
+                    "001001",
+                    "100011",
+                    "011001",
+                    "000110",
+                    "010101",
+                    "100110",
+                    "011001",
+                    "110000",
+                ],
+            ],
+        ],
+    )
+    def test_convert_bit_series(self, string: str, want: list):
+        got = Base64.convert_bit_series(string)
+
+        assert got == want
+
+    @pytest.mark.parametrize(
+        "string,want",
+        [
+            ("a", "YQ=="),
+            ("ab", "YWI="),
+            ("abc", "YWJj"),
+            ("hoge", "aG9nZQ=="),
+            ("hogehigehage", "aG9nZWhpZ2VoYWdl"),
+            ("hogehagehige", "aG9nZWhhZ2VoaWdl"),
+            ("EncodeToBase64Formatting", "RW5jb2RlVG9CYXNlNjRGb3JtYXR0aW5n"),
+        ],
+    )
+    def test_base64encode(self, string: str, want: str):
+        got = Base64.base64encoding(string)
+        assert got == want
+
+    @pytest.mark.parametrize(
+        "string,want",
+        [
+            ("YQ==", "a"),
+            ("YWI=", "ab"),
+            ("YWJj", "abc"),
+            ("aG9nZQ==", "hoge"),
+            ("aG9nZWhpZ2VoYWdl", "hogehigehage"),
+            ("aG9nZWhhZ2VoaWdl", "hogehagehige"),
+            ("RW5jb2RlVG9CYXNlNjRGb3JtYXR0aW5n", "EncodeToBase64Formatting"),
+        ],
+    )
+    def test_base64decode(self, string: str, want: str):
+        got = Base64.base64decoding(string)
+        assert got == want
