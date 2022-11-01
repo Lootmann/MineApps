660356b322e9251337d65a3918886e889b08a9c1
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-11-01 09:44:16 +0900

    misc: yaml

--- misc/yaml/myyaml.py
+++ misc/yaml/myyaml.py
@@ -1,2 +1,50 @@
-def load(text: str) -> dict:
-    return {}
+"""
+<empty> := ""
+<char>  := a-zA-Z
+<int>   := 0-9
+<elem>  := <char> | <int> | <empty>
+"""
+
+
+def check_type(line: str) -> str:
+    """
+    when line is dict, line should have '<char> :'
+    when line is array, line should have '- <elem>'
+    """
+
+    # NOTE: dict can nest
+    if ":" in line:
+        s = line.split(":")
+        if len(s) == 2:
+            return "dict"
+
+    if line[0] == "-":
+        return "array"
+
+    raise ValueError("Invalid Yaml")
+
+
+def convert_value(elem: str) -> str | int:
+    try:
+        return int(elem)
+    except:
+        return elem.strip()
+
+
+def load(text: str) -> dict | list | None:
+    if text == "":
+        return None
+
+    d = dict()
+
+    for _, line in enumerate(text.rstrip().split("\n")):
+        token_type = check_type(line)
+
+        if token_type == "dict":
+            left, right = line.split(":")
+            d[left] = convert_value(right)
+
+        if token_type == "array":
+            pass
+
+    return d
--- misc/yaml/test_myyaml.py
+++ misc/yaml/test_myyaml.py
@@ -1,19 +1,58 @@
 from pathlib import Path
+from typing import Tuple
 
 import pytest
+
+import myyaml
+import yaml
 
 
 class TestMyYaml:
     @pytest.fixture(autouse=True)
     def initial(self):
+        """
+        create tests
+
+        yaml names are:
+            array boolean dict empty
+            int int_minus mixed string
+
+        filename: { text: str, yaml: dict }
+        """
         yamls_dir = Path("./yamls").expanduser()
 
-        # filename, filepath
-        self.paths = {}
+        self.files = {}
 
         for p in yamls_dir.glob("**/*.yaml"):
-            self.paths[p.stem] = p.read_text()
+            self.files[p.stem] = {
+                "text": p.read_text(),
+                "yaml": yaml.safe_load(p.read_text()),
+            }
 
+    def split_tests(self, filename: str) -> Tuple[str, dict]:
+        return self.files[filename]["text"], self.files[filename]["yaml"]
+
+    # def test_show(self):
+    #     for k, v in self.files.items():
+    #         print("============", k)
+    #         print(v["text"])
+    #         print(v["yaml"])
+    #         print()
+    #
     def test_empty_yaml(self):
-        for filename, o in self.paths.items():
-            print(filename, o)
+        text, want = self.split_tests("empty")
+        got = myyaml.load(text)
+
+        assert want == got
+
+    def test_int(self):
+        text, want = self.split_tests("int")
+        got = myyaml.load(text)
+
+        assert want == got
+
+    def test_string(self):
+        text, want = self.split_tests("string")
+        got = myyaml.load(text)
+
+        assert want == got
