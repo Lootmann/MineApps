1628df80b7c43135f4d94e02e77a7ee06a52d652
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-12-18 22:46:08 +0900

    household: pages:views aggregates HouseholdModel

--- misc/household/app/pages/views.py
+++ misc/household/app/pages/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.views import generic
 
 from households.models import HouseHoldModel
@@ -9,3 +10,10 @@ class PagesIndexView(generic.ListView):
     context_object_name = "households"
     ordering = ("-registered_at",)
     paginate_by = 15
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["household_aggregates"] = HouseHoldModel.objects.values("category__name").annotate(
+            total_cost=Sum("cost")
+        )
+        return context
