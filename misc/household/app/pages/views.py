40ff5825db0bd3187e9067c81181bc36e842482a
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-01 11:38:53 +0900

    household: pages.Index.View show two months, last, this month aggretations

--- misc/household/app/pages/views.py
+++ misc/household/app/pages/views.py
@@ -1,7 +1,19 @@
 from django.db.models import Sum
+from django.utils import timezone
 from django.views import generic
 
 from households.models import HouseHoldModel
+
+
+def get_aggregate_households(year: int, month: int):
+    aggregations = (
+        HouseHoldModel.objects.filter(registered_at__year=year, registered_at__month=month)
+        .values("category__name")
+        .annotate(total_cost=Sum("cost"))
+        .order_by("category__name")
+    )
+
+    return aggregations
 
 
 class PagesIndexView(generic.ListView):
@@ -13,7 +25,27 @@ class PagesIndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["household_aggregates"] = HouseHoldModel.objects.values("category__name").annotate(
-            total_cost=Sum("cost")
+        context["household_aggregates"] = (
+            HouseHoldModel.objects.values("category__name")
+            .annotate(total_cost=Sum("cost"))
+            .order_by("category__name")
         )
+
+        # get two months ago, last month and this month aggregations
+        this_month = timezone.now()
+        last_month = this_month.replace(day=1) - timezone.timedelta(days=1)
+        two_month_ago = last_month.replace(day=1) - timezone.timedelta(days=1)
+
+        context["this_month"] = get_aggregate_households(
+            year=this_month.year, month=this_month.month
+        )
+
+        context["last_month"] = get_aggregate_households(
+            year=last_month.year, month=last_month.month
+        )
+
+        context["two_month"] = get_aggregate_households(
+            year=two_month_ago.year, month=two_month_ago.month
+        )
+
         return context
