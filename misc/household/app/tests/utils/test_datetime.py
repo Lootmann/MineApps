cd7c2375f9089282c517ad6e89409215d69ca5f9
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-01 22:02:17 +0900

    household: test test_datetime.py remove useless print

--- misc/household/app/tests/utils/test_datetime.py
+++ misc/household/app/tests/utils/test_datetime.py
@@ -5,12 +5,9 @@ class TestDatetime:
     def test_get_last_month(self):
         # get the first datetime current month
         this_month = timezone.now().replace(day=1)
-        print(this_month)
-        print(this_month.year, this_month.month)
 
         # this month - 1 day = the last day of last month
         last_month = this_month - timezone.timedelta(days=1)
-        print(last_month.year, last_month.month)
 
         if this_month.month == 1:
             assert this_month.year == last_month.year + 1 and last_month.month == 12
